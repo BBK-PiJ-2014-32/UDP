ff08197f2b8eb78bb672b7fc21b555cd00de68db
Phil Hannant <philip.a.hannant@gmail.com>
2015-04-22 18:10:37 +0100

    sendUniqueId added

--- src/udp/ServerClientHandlerImpl.java
+++ src/udp/ServerClientHandlerImpl.java
@@ -1,17 +1,31 @@
 package udp;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
 public class ServerClientHandlerImpl implements ServerClientHandler, Runnable {
 
+	private static int clientId = 0;
+	private Socket client;
+	
+	public ServerClientHandlerImpl(Socket client){
+		this.client = client;
+	}
+	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+	
 	}
 
 	@Override
-	public int sendUniqueId() {
-		// TODO Auto-generated method stub
-		return 0;
+	public void sendUniqueId() throws IOException, InterruptedException {
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+		clientId++;
+		writer.write(clientId);
+		writer.flush();
+		writer.close();
 	}
-
+	
 }
