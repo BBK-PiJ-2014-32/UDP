0f13282c163bb12b81f737d996c6547451b5072c
Phil Hannant <philip.a.hannant@gmail.com>
2015-04-22 12:02:48 +0100

    Basic description added to Client interface

--- src/udp/Client.java
+++ src/udp/Client.java
@@ -1,5 +1,12 @@
 package udp;
 
+/**
+ * The Client will connect to the server via TCP and while doing so will request a uniqueId.
+ * Once it has received this Id it will then ask if it is the first to connect, if it is it
+ * will open a UDP connection to the server and start sending an audio file. If it is not the first 
+ * it will listen for audio chunks coming down via UDP and play these chunks.
+ */
 public interface Client {
 
+	void connect();
 }
--- src/udp/Server.java
+++ src/udp/Server.java
@@ -1,6 +1,6 @@
 package udp;
 /**
-* The Interface for the Server, which will start listening for clients via TCP. 
+* The Server will start listening for clients via TCP. 
 * When a client connects the server will place the client connection and further 
 * handling of the client in a separate thread.
 * 
