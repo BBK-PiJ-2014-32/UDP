b61fe89a57ac86e273d04c2d3cb707642a77b634
Phil Hannant <philip.a.hannant@gmail.com>
2015-04-22 11:02:13 +0100

    Constructor added.

--- src/udp/ServerImpl.java
+++ src/udp/ServerImpl.java
@@ -1,7 +1,16 @@
 package udp;
 
-public class ServerImpl implements Server {
+import java.net.ServerSocket;
 
+public class ServerImpl implements Server {
+	
+	private ServerSocket serverSocket;
+	private int port;
+
+	public ServerImpl(int port){
+		this.port = port;
+	}
+	
 	@Override
 	public void listenForClients() {
 		// TODO Auto-generated method stub
