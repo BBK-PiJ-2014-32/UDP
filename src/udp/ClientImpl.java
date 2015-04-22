ae897dde75510a2e06ced8f58135f783d992d2d7
Phil Hannant <philip.a.hannant@gmail.com>
2015-04-22 12:07:19 +0100

    constructor added.

--- src/udp/ClientImpl.java
+++ src/udp/ClientImpl.java
@@ -2,6 +2,14 @@ package udp;
 
 public class ClientImpl implements Client{
 
+	private String hostName;
+	private int port;
+	
+	public ClientImpl(String host, int port){
+		this.hostName = host;
+		this.port = port;
+	}
+	
 	@Override
 	public void connect() {
 		// TODO Auto-generated method stub
