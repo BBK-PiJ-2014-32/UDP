8b162bc6b1970d5186d031ccde05d686dbba3f31
Phil Hannant <philip.a.hannant@gmail.com>
2015-04-22 16:56:15 +0100

    first commit

---
+++ src/udp/ServerClientHandlerImpl.java
@@ -0,0 +1,17 @@
+package udp;
+
+public class ServerClientHandlerImpl implements ServerClientHandler, Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int sendUniqueId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
