590e58859ccb9bdd0ccd7a27a0c06e2e1044370c
Phil Hannant <philip.a.hannant@gmail.com>
2015-04-22 10:03:39 +0100

    Interface description added

--- src/udp/Server.java
+++ src/udp/Server.java
@@ -1,5 +1,11 @@
 package udp;
-
+/**
+* The Interface for the Server, which will start listening for clients via TCP. 
+* When a client connects the server will place the client connection and further 
+* handling of the client in a separate thread.
+* 
+* @author P Hannant
+*/
 public interface Server {
 
 }
