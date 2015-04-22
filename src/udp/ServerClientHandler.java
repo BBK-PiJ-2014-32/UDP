1f5197532ff9623a6a57a962751d88a5964390a8
Phil Hannant <philip.a.hannant@gmail.com>
2015-04-22 16:52:53 +0100

    Interface updated.

--- src/udp/ServerClientHandler.java
+++ src/udp/ServerClientHandler.java
@@ -1,5 +1,12 @@
 package udp;
 
+/**
+ * The ServerClientHandler sends the client the unique Id and then indicates if that client is a sender or 
+ * receiver process. Then listens for UDP connection, then tells the client to connect over UDP and then
+ * relays the audio data.
+ */
 public interface ServerClientHandler {
 
+	int sendUniqueId();
+	
 }
