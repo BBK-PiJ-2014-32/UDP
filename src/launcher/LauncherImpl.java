037c138525f8d3de8334f124f2021ba152d37a11
Phil Hannant <philip.a.hannant@gmail.com>
2015-04-26 19:45:39 +0100

    javadoc added

--- src/launcher/LauncherImpl.java
+++ src/launcher/LauncherImpl.java
@@ -4,10 +4,20 @@ import java.util.Scanner;
 
 import udp.*;
 
+/**
+ * The implementation of the launcher interface.
+ * @see launcher.Launcher
+ */
 public class LauncherImpl implements Launcher{
 	
+	/** The scanner. */
 	private Scanner scan;
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args){
 		
 		
@@ -15,16 +25,21 @@ public class LauncherImpl implements Launcher{
 		tc.run();
 	}
 
+	/**
+	 * The run method which gives a brief explanation of the program and what it
+	 * will demonstrate.
+	 */
 	public void run(){
 		System.out.println("********************************************************");
-		System.out.println("* Welcome to my TCP/UDP file transfer program.         *");
-		System.out.println("* The program starts a server and then you can create  *");
-		System.out.println("* create some clients, the first client will send      *");
-		System.out.println("* some audio to the server, once the server has this   *");
-		System.out.println("* it will relay it back to any subsequent clients      *"); 
-		System.out.println("* that join. These clients will then play the audio    *"); 
-		System.out.println("* received. The launcher will create and run 10 clients*");
-		System.out.println("* and once then close once all audio has been recevied *");
+		System.out.println("* TCP/UDP file transfer program.                       *");
+		System.out.println("* Before running this program ensure that the server   *");
+		System.out.println("* has been launched. Once the server is running this   *");
+		System.out.println("* program will iniate the clients which will send audio*");
+		System.out.println("* to the server, once the server has this it will relay*");
+		System.out.println("* it back to any subsequent clients that join. These   *"); 
+		System.out.println("* clients will then play the audio received.           *"); 
+		System.out.println("* The launcher will create and run 10 clients and then *");
+		System.out.println("* close once all audio has been recevied.              *");
 		System.out.println("********************************************************");
 		scan = new Scanner(System.in);
 		System.out.println("* Type 'Yes' to begin: ");	
@@ -37,6 +52,9 @@ public class LauncherImpl implements Launcher{
 		
 	}
 		
+	/**
+	 * @see launcher.Launcher#createClient()
+	 */
 	public void createClient(){
 		try {
 			for(int i = 0; i < 10; i++){
