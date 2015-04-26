fbdc4ae0de5abeb2e0732391b28599c5f4b0397e
Phil Hannant <philip.a.hannant@gmail.com>
2015-04-26 16:27:31 +0100

    startserver and createCleint methods added to launcher

--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,20 +1,39 @@
 package launcher;
 
+import java.io.IOException;
+
 import udp.*;
 
 public class Launcher {
+	
+	private static final String[] args = {};
 
 	public static void main(String[] args){
+		
+		
 		Launcher tc = new Launcher();
 		tc.run();
 	}
 
 	public void run(){
+		startServer();
 		ClientImpl client1 = new ClientImpl("localHost", 2000);
 		client1.run();
-		//ClientImpl client2 = new ClientImpl("localHost", 3000);
-		//client2.run();
 	
 	}
-
+	
+	@SuppressWarnings("static-access")
+	public static void startServer(){
+		try {
+			ServerImpl server = new ServerImpl(2000);
+			server.main(args);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public void createClient(){
+		ClientImpl client = new ClientImpl("localHost", 2000);
+		client.run();
+	}
 }
