2cb65b8eac1d51a1de000b53ac3af7e5bdd265bb
Phil Hannant <philip.a.hannant@gmail.com>
2015-04-26 19:12:14 +0100

    Interface updated.

--- src/udp/Client.java
+++ src/udp/Client.java
@@ -2,6 +2,7 @@ package udp;
 
 import java.net.Socket;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Client will connect to the server via TCP and while doing so will request a uniqueId.
  * Once it has received this Id it will then ask if it is the first to connect, if it is it
@@ -10,17 +11,43 @@ import java.net.Socket;
  */
 public interface Client {
 
+	/**
+	 * Connects to the server via a TCP socket connection.
+	 */
 	void connectToServerViaTCP();
 	
+	/**
+	 * Requests a unique id from the server, which is used to allocate what process
+	 * the client will be carrying out.
+	 *
+	 * @param the socket which the client is connected on. 
+	 */
 	void requestUniqueId(Socket client);
 	
+	/**
+	 * Checks if is first to connect and if it is the client is allocated as the sender..
+	 */
 	void isFirstToConnect();
 	
-	void recieveInstructionForUDP();
+	/**
+	 * Receives the instruction from the server to connect over UDP.
+	 */
+	void receiveInstructionForUDP();
 	
+	/**
+	 * Sends the audio file over UDP to the server.
+	 */
 	void sendViaUDP();
 	
+	/**
+	 * Receives the byte array from the server via UDP.
+	 */
 	void receiveViaUDP();
 	
+	/**
+	 * Plays the byte array that has been sent from the server.
+	 *
+	 * @param the byte array to be played.
+	 */
 	void playAudio(byte[] audioBytes);
 }
