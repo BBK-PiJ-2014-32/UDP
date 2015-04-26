563b7e404739efd20a460fec2f08ed451de31aea
Phil Hannant <philip.a.hannant@gmail.com>
2015-04-26 19:47:29 +0100

    javadoc added

--- src/launcher/ServerLauncher.java
+++ src/launcher/ServerLauncher.java
@@ -1,7 +1,15 @@
 package launcher;
 
+/**
+ * The ServerLauncher initializes the server program to be used by the clients.
+ */
 public interface ServerLauncher {
 	
+	/**
+	 * Starts the server to be used.
+	 * 
+	 * @exception IOException
+	 */
 	void startServer();
 
 }
