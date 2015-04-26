91898b49d27fd8c42d4f3d54444ff17b96b40396
Phil Hannant <philip.a.hannant@gmail.com>
2015-04-26 19:50:29 +0100

    javadoc added

--- src/launcher/ServerLauncherImpl.java
+++ src/launcher/ServerLauncherImpl.java
@@ -4,16 +4,33 @@ import java.io.IOException;
 
 import udp.ServerImpl;
 
+/**
+ * The Server Launcher class.
+ * @see launcher.ServerLauncher
+ */
 public class ServerLauncherImpl implements ServerLauncher{
 	
+	/** The server object. */
 	private ServerImpl server;
+	
+	/** The empty arguments used to run the servers main method. */
 	private String [] args = {};
 
+	/**
+	 * The main method used to run the start the server method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args){
 		ServerLauncherImpl sl = new ServerLauncherImpl();
 		sl.startServer();
 	}
 	
+	/**
+	 * Starts the server.
+	 *
+	 * @see launcher.ServerLauncher#startServer()
+	 */
 	@Override
 	@SuppressWarnings("static-access")
 	public void startServer(){
@@ -24,7 +41,5 @@ public class ServerLauncherImpl implements ServerLauncher{
 			ex.printStackTrace();
 		}
 	}
-	
-	
 
 }
