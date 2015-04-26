9b0dbe4a752892c0777dfe6fc0ac53c16078c4ee
Phil Hannant <philip.a.hannant@gmail.com>
2015-04-26 16:52:52 +0100

    run method intro added.

--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,12 +1,16 @@
 package launcher;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import udp.*;
 
 public class Launcher {
 	
 	private static final String[] args = {};
+	private Scanner scan;
+	private boolean running;
+	private int count = 0;
 
 	public static void main(String[] args){
 		
@@ -16,10 +20,33 @@ public class Launcher {
 	}
 
 	public void run(){
-		startServer();
-		ClientImpl client1 = new ClientImpl("localHost", 2000);
-		client1.run();
-	
+		System.out.println("*******************************************************");
+		System.out.println("* Welcome to my TCP/UDP file transfer program.        *");
+		System.out.println("* The program starts a server and then you can create *");
+		System.out.println("* create some clients, the first client will send     *");
+		System.out.println("* some audio to the server, once the server has this  *");
+		System.out.println("* it will relay it back to any subsequent clients     *"); 
+		System.out.println("* that join. These clients will then play the audio   *"); 
+		System.out.println("* received. A maximum of 10 clients can join.         *");
+		System.out.println("* There are 3 options to choose from:                 *");
+		System.out.println("* Press '1' to start the server.                      *");
+		System.out.println("* Press '2' to create a client.                       *");
+		System.out.println("* Press '9' to close.                                 *");
+		System.out.println("*******************************************************");
+		scan = new Scanner(System.in);
+		running = true;
+		while(!running){
+			System.out.println("");
+			if(scan.next().equals("1")){
+				startServer();
+			} else if (scan.next().equals("2")){
+				createClient();
+				count++;
+			} else if (scan.next().equals("9")|| count == 10){
+				running = false;
+			}
+		}
+		
 	}
 	
 	@SuppressWarnings("static-access")
@@ -27,6 +54,7 @@ public class Launcher {
 		try {
 			ServerImpl server = new ServerImpl(2000);
 			server.main(args);
+			System.out.println("Server started.");
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
@@ -35,5 +63,9 @@ public class Launcher {
 	public void createClient(){
 		ClientImpl client = new ClientImpl("localHost", 2000);
 		client.run();
+	}
+	
+	public void closeServer(){
+		///
 	}
 }
