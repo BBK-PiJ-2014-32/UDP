c6a49c0d85924f1fc478680ac46f904ef2bdbb1d
Phil Hannant <philip.a.hannant@gmail.com>
2015-04-26 19:38:42 +0100

    javadoc added.

--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,7 +1,14 @@
 package launcher;
 
+/**
+ * The launcher is used to create and run the clients that connect to the server, send the audio
+ * and play the audio bytes received..
+ */
 public interface Launcher {
 
+	/**
+	 * Creates the clients and initiates the each clients run method..
+	 */
 	void createClient();
 	
 }
