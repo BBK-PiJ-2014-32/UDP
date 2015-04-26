a9d9d1e644540980429689673ba29c4dbcee2545
Phil Hannant <philip.a.hannant@gmail.com>
2015-04-26 19:26:58 +0100

    javadoc added

--- src/udp/ServerClientHandler.java
+++ src/udp/ServerClientHandler.java
@@ -10,12 +10,36 @@ import java.net.SocketException;
  */
 public interface ServerClientHandler {
 
+	/**
+	 * Sends a unique id to the client that has requested it.
+	 *
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 * @throws InterruptedException the interrupted exception
+	 */
 	void sendUniqueId() throws IOException, InterruptedException;
 	
+	/**
+	 * Notifies the client that has connected if they are the first to connect
+	 * in order to allocate the correct process to that client.
+	 *
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
 	void notifyClientIfFirst() throws IOException;
 	
+	/**
+	 * Listens for any incoming data sent over UDP through the server.
+	 *
+	 * @throws SocketException the socket exception
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
 	void listenForUDP() throws SocketException, IOException;
 	
+	/**
+	 * Tells the client to connect over UDP so that the audio data can
+	 * begin to be sent.
+	 *
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
 	void tellClientToConnectOnUDP()  throws IOException;
 	
 	
